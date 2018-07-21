9d243b730df0b6602b962cba52df5d240acfd61b
Alejandro Santander <Palebluedot@gmail.com>
2018-07-21 19:20:18 -0300

    Working textarea highlights using composited view

--- src/components/CustomTextarea.js
+++ src/components/CustomTextarea.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import _ from 'lodash';
 
 class CustomTextarea extends Component {
 
@@ -13,47 +12,49 @@ class CustomTextarea extends Component {
     this.setState({
       content: this.props.initialContent
     });
+    this.updateHighlights(this.props.initialContent);
   }
 
   updateContent(content) {
     this.setState({ content });
     this.props.updateCallback(content);
+    this.updateHighlights(content);
   }
 
-  applyMarkHighlights(content) {
+  updateHighlights(content) {
     const range = {
       start: 2,
-      end: 50
+      end: 150
     }
     const pre = content.substring(0, range.start);
     const con = `<mark>${content.substring(range.start, range.end)}</mark>`
     const pos = content.substring(range.end);
-    return pre + con + pos;
-  }
-
-  wrapContent(content) {
-    // content = _.escape(content);
-    // content = content.replace(/\r\n/g, '<br>')
-    // content = this.applyMarkHighlights(content);
-    // content = `<div>${content}</div>`;
-    return content;
-  }
-
-  unwrapContent(content) {
-    // content = content.replace(/<[^>]*>/g, '');
-    // content = content.replace(/<br>/g, '\n');
-    // content = _.unescape(content);
-    return content;
+    this.refs.highlights.innerHTML = pre + con + pos;
   }
 
   render() {
     return (
       <div className='container'>
 
+        {/* 
+          NOTE: The technique used below for custom highlights in a textarea
+          is based on creating a div copy of the textarea behind it, and editing
+          its html content with mark tags. It is extremely fragile in terms that the textarea
+          and the div need to look identical with css. Tried other options like react components
+          and contenteditable divs and they are full of quirks. 
+          The technique used here is ugly, but works.
+        */}
+
+        {/* Underlay dummy just for highlights. */}
+        <div className="backdrop">
+          <div class="highlights" ref="highlights"> </div>
+        </div>
+
+        {/* Visible/editable text. */}
         <textarea
           className='editable_content'
-          onChange={evt => this.updateContent(this.unwrapContent(evt.target.value))}
-          value={this.wrapContent(this.state.content)}
+          onChange={evt => this.updateContent(evt.target.value)}
+          value={this.state.content}
         />
 
       </div>
