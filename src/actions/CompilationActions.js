094be7dff5177f8300ed60e313632aeb8c700519
Alejandro Santander <Palebluedot@gmail.com>
2018-07-23 15:46:59 -0300

    Removed server

--- src/actions/CompilationActions.js
+++ src/actions/CompilationActions.js
@@ -1,6 +1,5 @@
 import * as ActionTypes from './ActionTypes';
 import Store from '../store';
-import axios from 'axios';
 import Disassembler from '../utils/DisassemblerUtil';
 import CompilerUtil from '../utils/CompilerUtil';
 import MappingActions from './MappingActions';
@@ -36,43 +35,6 @@ const CompilationActions = {
       // Compilation resets source mappings.
       dispatch(MappingActions.outputSelected({start: 0, end: 0}));
       dispatch(MappingActions.sourceSelected({start: 0, end: 0}));
-
-      // Use the server to compile using native solc.
-      // axios.post(
-      //   'http://localhost:1337',
-      //   { source }
-      // )
-      // .then(response => {
-
-      //   const resp = response.data
-
-      //   // Parse output.
-      //   let output; 
-      //   let srcmap;
-      //   if(resp.errors) output = resp.errors; // Just errors.
-      //   else {
-
-      //     // Parse json compilation results for binary and sourcemap.
-      //     output = JSON.parse(resp.output);
-          
-      //     // Assuming there is only one contract, so get any key.
-      //     let contract;
-      //     for(var key in output.contracts) {
-      //       contract = output.contracts[key];
-      //     }
-      //     output = contract['bin-runtime'];
-      //     srcmap = contract['srcmap-runtime'];
-
-      //     // Disassemble.
-      //     output = Disassembler.disassemble(output);
-      //   }
-
-      //   dispatch(CompilationActions.sourceCompiled(output, srcmap || ''));
-
-      //   // Compilation resets source mappings.
-      //   dispatch(MappingActions.outputSelected({start: 0, end: 0}));
-      //   dispatch(MappingActions.sourceSelected({start: 0, end: 0}));
-      // })
     }
   },
 
